a1f51bef9c2aab6318700c6e9625e1b1e0f52a0d
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-15 12:36:19 +0200

    authcontextprovider aangepast met jwt token maar error login is not a function blijft.

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,11 +1,151 @@
-import {createContext, useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {jwtDecode} from 'jwt-decode';
+// import {createContext, useEffect, useState} from 'react';
+// import {useNavigate} from 'react-router-dom';
+// import axios from 'axios';
+// import tokenExpired from "../helpers/tokenExpired.jsx";
+// import tokenUsername from "../helpers/tokenUsername.jsx";
+//
+// export const AuthContext = createContext({});
+//
+// const AuthContextProvider = ({children}) => {
+//     const [isAuth, toggleIsAuth] = useState({
+//         isAuth: false,
+//         user: null,
+//         status: 'pending',
+//     });
+//
+//     useEffect(() => {
+//         const jwt = localStorage.getItem('jwt');
+//         if (jwt && tokenExpired(jwt)) {
+//             const username = tokenUsername(jwt);
+//             void login (jwt, username);
+//         } else {
+//             logout();
+//         }
+//     }, []);
+//
+//     const navigate = useNavigate();
+//     // eslint-disable-next-line no-unused-vars
+//     const [error, toggleError] = useState(false);
+//     // eslint-disable-next-line no-unused-vars
+//     const [loading, toggleLoading] = useState(false);
+//
+//     async function authenticate(username, password) {
+//         toggleError(false);
+//         toggleLoading(true);
+//
+//         try {
+//             const result = await axios.post('http://localhost:1991/login', {
+//                 username: username,
+//                 password: password
+//             });
+//             console.log(result.data);
+//             localStorage.setItem('jwt', result.data.jwt);
+//             navigate('/profile');
+//         } catch (e) {
+//             console.error(e);
+//             toggleError(true);
+//         } finally {
+//             toggleLoading(false);
+//     }
+//
+//     async function login(jwt, username) {
+//             toggleLoading(true);
+//             try {
+//                 const result = await axios.get(`http://localhost:1991/users/${username}`, {
+//                     headers: {
+//                         "Content-Type": "application/json",
+//                         Authorization: `Bearer ${jwt}`,
+//                     },
+//                 });
+//                 toggleIsAuth({
+//                     isAuth: true,
+//                     user: {
+//                         username: result.data.username,
+//                     },
+//                     status: 'done',
+//                 });
+//                 localStorage.setItem('jwt', jwt);
+//             } catch (e) {
+//                 console.error(e);
+//                 toggleIsAuth({
+//                     isAuth: false,
+//                     user: null,
+//                     status: 'done',
+//                 });
+//             } finally {
+//                 toggleLoading(false);
+//             }
+//     }
+//     }
+//
+//     // function login(jwt) {
+//     //     localStorage.setItem('jwt', jwt);
+//     //     const decoded = jwtDecode(token);
+//     //     void fetchUserData(decoded.sub, token);
+//     // }
+//
+//     function logout() {
+//         localStorage.clear();
+//         toggleIsAuth({
+//             isAuth: false,
+//             user: null,
+//             status: 'done',
+//         });
+//         console.log('Gebruiker is uitgelogd!');
+//         navigate('/');
+//     }
+//
+//     // async function fetchUserData(username, token) {
+//     //     try {
+//     //         const result = await axios.get( `http://localhost:1991/users/${username}`, {
+//     //             headers: {
+//     //                 "Content-Type": "application/json",
+//     //                 Authorization: `Bearer ${token}`,
+//     //             },
+//     //         });
+//     //         toggleIsAuth( {
+//     //             isAuth: true,
+//     //             user: {
+//     //                 username: result.data.username,
+//     //                 // email: result.data.email,
+//     //             },
+//     //             status: 'done',
+//     //         });
+//     //     } catch ( e ) {
+//     //         console.error( e );
+//     //         toggleIsAuth( {
+//     //             isAuth: false,
+//     //             user: null,
+//     //             status: 'done',
+//     //         });
+//     //     }
+//     // }
+//
+//     const contextData = {
+//         ...isAuth,
+//         authenticate,
+//         login,
+//         logout,
+//     };
+//
+//     return (
+//         <AuthContext.Provider value={{contextData}}>
+//             {isAuth.status === 'done' ? children : <p>Loading...</p>}
+//         </AuthContext.Provider>
+//     );
+// }
+//
+// export default AuthContextProvider;
+
+import { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import tokenExpired from "../helpers/tokenExpired.jsx";
+import tokenUsername from "../helpers/tokenUsername.jsx";
 
 export const AuthContext = createContext({});
 
-const AuthContextProvider = ({children}) => {
+const AuthContextProvider = ({ children }) => {
     const [isAuth, toggleIsAuth] = useState({
         isAuth: false,
         user: null,
@@ -13,25 +153,69 @@ const AuthContextProvider = ({children}) => {
     });
 
     useEffect(() => {
-        const token = localStorage.getItem('jwt');
-        if (token) {
-            const decoded = jwtDecode(token);
-            void fetchUserData(decoded.sub, token);
+        const jwt = localStorage.getItem('jwt');
+        if (jwt && !tokenExpired(jwt)) {
+            const username = tokenUsername(jwt);
+            void login(jwt, username);
         } else {
+            logout();
+        }
+    }, []);
+
+    const navigate = useNavigate();
+    // eslint-disable-next-line no-unused-vars
+    const [error, toggleError] = useState(false);
+    // eslint-disable-next-line no-unused-vars
+    const [loading, toggleLoading] = useState(false);
+
+    async function authenticate(username, password) {
+        toggleError(false);
+        toggleLoading(true);
+
+        try {
+            const result = await axios.post('http://localhost:1991/login', {
+                username: username,
+                password: password
+            });
+            console.log(result.data);
+            localStorage.setItem('jwt', result.data.jwt);
+            await login(result.data.jwt, username);
+            navigate('/profile');
+        } catch (e) {
+            console.error(e);
+            toggleError(true);
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
+    async function login(jwt, username) {
+        toggleLoading(true);
+        try {
+            const result = await axios.get(`http://localhost:1991/users/${username}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+            toggleIsAuth({
+                isAuth: true,
+                user: {
+                    username: result.data.username,
+                },
+                status: 'done',
+            });
+            localStorage.setItem('jwt', jwt);
+        } catch (e) {
+            console.error(e);
             toggleIsAuth({
                 isAuth: false,
                 user: null,
                 status: 'done',
             });
+        } finally {
+            toggleLoading(false);
         }
-    }, []);
-
-    const navigate = useNavigate();
-
-    function login(token) {
-        localStorage.setItem('jwt', token);
-        const decoded = jwtDecode(token);
-        void fetchUserData(decoded.sub, token);
     }
 
     function logout() {
@@ -45,40 +229,15 @@ const AuthContextProvider = ({children}) => {
         navigate('/');
     }
 
-    async function fetchUserData(username, token) {
-        try {
-            const result = await axios.get( `http://localhost:1991/users/${username}`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            toggleIsAuth( {
-                isAuth: true,
-                user: {
-                    username: result.data.username,
-                    // email: result.data.email,
-                },
-                status: 'done',
-            });
-        } catch ( e ) {
-            console.error( e );
-            toggleIsAuth( {
-                isAuth: false,
-                user: null,
-                status: 'done',
-            });
-        }
-    }
-
     const contextData = {
         ...isAuth,
+        authenticate,
         login,
-        logout
+        logout,
     };
 
     return (
-        <AuthContext.Provider value={{contextData}}>
+        <AuthContext.Provider value={contextData}>
             {isAuth.status === 'done' ? children : <p>Loading...</p>}
         </AuthContext.Provider>
     );
