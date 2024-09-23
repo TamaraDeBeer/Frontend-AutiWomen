841a2e803a4ca02a3447c8e824c89009c2b15015
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-23 19:11:43 +0200

    useEffect en AccountProfile uitgebreid met forums van username

--- src/pages/accountProfile/AccountProfile.jsx
+++ src/pages/accountProfile/AccountProfile.jsx
@@ -3,9 +3,15 @@ import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../../context/AuthContextProvider.jsx";
 import axios from "axios";
 import calculateAge from "../../helpers/calculateAge.jsx";
+import ForumPostShort from "../../components/forumPostShort/ForumPostShort.jsx";
+import anna from "../../assets/profilePhoto/anna.jpg";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage.jsx";
 
 function AccountProfile() {
     const [profile, setProfile] = useState({});
+    const [forums, setForums] = useState([]);
+    // eslint-disable-next-line no-unused-vars
+    const [error, toggleError] = useState(false);
     const {user} = useContext(AuthContext);
 
     useEffect(() => {
@@ -15,18 +21,29 @@ function AccountProfile() {
             console.log(username);
 
             try {
-                const result = await axios.get(`http://localhost:1991/users/${username}`, {
+                const profileResult = await axios.get(`http://localhost:1991/users/${username}`, {
                     headers: {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${jwt}`,
                     },
                 });
-                setProfile(result.data);
-                console.log(result.data);
+                setProfile(profileResult.data);
+                console.log(profileResult.data);
+
+                const forumsResult = await axios.get(`http://localhost:1991/users/${username}/forums`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${jwt}`,
+                    },
+                });
+                setForums(forumsResult.data);
+                console.log(forumsResult.data);
+
             } catch (e) {
                 console.error(e);
             }
         }
+
         void fetchProfileData();
     }, [])
 
@@ -59,6 +76,48 @@ function AccountProfile() {
             <section className={styles['outer-container']}>
                 <div className={styles['innerContainer']}>
                     <h2>Jouw Forums</h2>
+
+                    {forums.length > 0 ? (
+                        forums.map((forum) => (
+                            <ForumPostShort
+                                key={forum.id}
+                                image={anna}
+                                name={forum.name}
+                                age={calculateAge(forum.age) + ' jaar'}
+                                title={forum.title}
+                                text={forum.text.split(' ').slice(0, 40).join(' ')}
+                                link={`/forum/${forum.id}`}
+                                likes={forum.likes}
+                                comments={forum.comments}
+                                views={forum.views}
+                                lastReaction={forum.lastReaction}
+                            />
+                        ))
+                    ) : (
+                        <p>Geen forums gevonden.</p>
+                    )}
+                    {error && <ErrorMessage message="Er is iets misgegaan bij het ophalen van de data. Probeer het opnieuw." />}
+
+                    {/*{Object.keys(forums).length > 0 &&*/}
+
+                    {/*    forums.map((forum) => {*/}
+                    {/*            return <ForumPostShort*/}
+                    {/*                key={forum.id}*/}
+                    {/*                image={anna}*/}
+                    {/*                name={forum.name}*/}
+                    {/*                age={calculateAge(forum.age) + ' jaar'}*/}
+                    {/*                title={forum.title}*/}
+                    {/*                text={forum.text.split(' ').slice(0, 40).join(' ')}*/}
+                    {/*                link={`/forum/${forum.id}`}*/}
+                    {/*                likes={forum.likes}*/}
+                    {/*                comments={forum.comments}*/}
+                    {/*                views={forum.views}*/}
+                    {/*                lastReaction={forum.lastReaction}*/}
+                    {/*            />*/}
+                    {/*        }*/}
+                    {/*    )}*/}
+                    {/*{error && <ErrorMessage*/}
+                    {/*    message="Er is iets misgegaan bij het ophalen van de data. Probeer het opnieuw."/>}*/}
                 </div>
             </section>
 
