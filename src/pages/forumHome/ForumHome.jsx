401544e5540289f88f1f3e87ce8c6b09bec048f2
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-10-08 15:47:12 +0200

    forumHome aangepast met slider nieuwste / trending die de forums respectievelijk sorteert

--- src/pages/forumHome/ForumHome.jsx
+++ src/pages/forumHome/ForumHome.jsx
@@ -1,10 +1,10 @@
+import {useEffect, useState} from 'react';
 import styles from './ForumHome.module.css';
 import Button from "../../components/button/Button.jsx";
 import search from "../../assets/logo/search.png";
 import {useNavigate} from "react-router-dom";
 import ForumPostShort from "../../components/forumPostShort/ForumPostShort.jsx";
 import axios from 'axios';
-import {useEffect, useState} from "react";
 import ErrorMessage from "../../components/errorMessage/ErrorMessage.jsx";
 import calculateAge from "../../helpers/calculateAge.jsx";
 
@@ -12,88 +12,97 @@ function ForumHome() {
     const navigate = useNavigate();
     const [forums, setForums] = useState([]);
     const [error, toggleError] = useState(false);
-
+    const [sliderOption, setSliderOption] = useState('newest');
 
     useEffect(() => {
-        void fetchAllForums();
-    }, []);
+        fetchAllForums();
+    }, [sliderOption]);
 
-        async function fetchAllForums() {
-            toggleError(false);
+    async function fetchAllForums() {
+        toggleError(false);
+        const endpoint = sliderOption === 'newest' ? 'http://localhost:1991/forums/sorted-by-date' : 'http://localhost:1991/forums/sorted-by-likes';
 
-            try {
-                const response = await axios.get('http://localhost:1991/forums');
-                setForums(response.data);
-                console.log(response.data);
-            } catch (e) {
-                console.error(e);
-                toggleError(true);
-            }
+        try {
+            const response = await axios.get(endpoint);
+            setForums(response.data);
+            console.log(response.data);
+        } catch (e) {
+            console.error(e);
+            toggleError(true);
         }
+    }
 
-    return (<>
-        <section className="outer-container">
-            <div className={`inner-container ${styles['section-forum__inner-container']}`}>
-                <h1>Auti-Women Forum</h1>
-                <h2>Deel je problemen, geef advies en wees respectvol</h2>
-                <div>
-                    <Button type="button" className={styles['button-forum']}
-                            onClick={() => navigate('/forum/create')}
-                    >Schrijf een forum</Button>
+    return (
+        <>
+            <section className="outer-container">
+                <div className={`inner-container ${styles['section-forum__inner-container']}`}>
+                    <h1>Auti-Women Forum</h1>
+                    <h2>Deel je problemen, geef advies en wees respectvol</h2>
+                    <div>
+                        <Button type="button" className={styles['button-forum']}
+                                onClick={() => navigate('/forum/create')}
+                        >Schrijf een forum</Button>
+                    </div>
+                    <div>
+                        <button className={styles['section-forum__button-search']}>Zoeken in alle forums.. <img
+                            src={search} alt="search logo"/></button>
+                    </div>
                 </div>
-                <div>
-                    <button className={styles['section-forum__button-search']}>Zoeken in alle forums.. <img src={search}
-                                                                                                            alt="search logo"/>
-                    </button>
-                </div>
-            </div>
-        </section>
-
-        <section className={styles['section-forum__main']}>
-            <section className={styles['section-forum__posts-short']}>
-
-                <div>
-                    <h3>slider nieuwste / trending</h3>
-                </div>
-
-                {forums.map((forum) => {
-                    return <ForumPostShort
-                        key={forum.id}
-                        image={forum.userDto?.profilePictureUrl}
-                        name={forum.name}
-                        age={calculateAge(forum.age) + ' jaar'}
-                        title={forum.title}
-                        date={forum.date}
-                        text={forum.text.split(' ').slice(0, 50).join(' ')}
-                        link={`/forum/${forum.id}`}
-                        likesCount={forum.likesCount}
-                        commentsCount={forum.commentsCount}
-                        viewsCount={forum.viewsCount}
-                        lastReaction={forum.lastReaction}
-                    />
-                })}
-                {error && <ErrorMessage message="Er is iets misgegaan bij het ophalen van de data. Probeer het opnieuw." />}
-
             </section>
 
-            <section className={styles['section-forum__sidebar']}>
-                <h2>Populaire Onderwerpen</h2>
-                <ul className={styles['section-forum__sidebar-list']}>
-                    <li>Fysieke Gezondheid</li>
-                    <li>Mentale Gezondheid</li>
-                    <li>Huishouden</li>
-                    <li>Werk</li>
-                    <li>School</li>
-                    <li>Relaties</li>
-                    <li>Vriendschappen</li>
-                    <li>Rouw</li>
-                </ul>
+            <section className={styles['section-forum__main']}>
+                <section className={styles['section-forum__posts-short']}>
+                    <div className={styles['slider']}>
+                        <button
+                            onClick={() => setSliderOption('newest')}
+                            className={`${styles['button']} ${styles['newest']} ${sliderOption === 'newest' ? styles['active'] : ''}`}
+                        >
+                            Nieuwste
+                        </button>
+                        <button
+                            onClick={() => setSliderOption('trending')}
+                            className={`${styles['button']} ${styles['trending']} ${sliderOption === 'trending' ? styles['active'] : ''}`}
+                        >
+                            Trending
+                        </button>
+                    </div>
+
+                    {forums.map((forum) => {
+                        return <ForumPostShort
+                            key={forum.id}
+                            image={forum.userDto?.profilePictureUrl}
+                            name={forum.name}
+                            age={calculateAge(forum.age) + ' jaar'}
+                            title={forum.title}
+                            date={forum.date}
+                            text={forum.text.split(' ').slice(0, 50).join(' ')}
+                            link={`/forum/${forum.id}`}
+                            likesCount={forum.likesCount}
+                            commentsCount={forum.commentsCount}
+                            viewsCount={forum.viewsCount}
+                            lastReaction={forum.lastReaction}
+                        />
+                    })}
+                    {error && <ErrorMessage
+                        message="Er is iets misgegaan bij het ophalen van de data. Probeer het opnieuw."/>}
+                </section>
+
+                <section className={styles['section-forum__sidebar']}>
+                    <h2>Populaire Onderwerpen</h2>
+                    <ul className={styles['section-forum__sidebar-list']}>
+                        <li>Fysieke Gezondheid</li>
+                        <li>Mentale Gezondheid</li>
+                        <li>Huishouden</li>
+                        <li>Werk</li>
+                        <li>School</li>
+                        <li>Relaties</li>
+                        <li>Vriendschappen</li>
+                        <li>Rouw</li>
+                    </ul>
+                </section>
             </section>
-
-        </section>
-
-
-    </>);
+        </>
+    );
 }
 
 export default ForumHome;
