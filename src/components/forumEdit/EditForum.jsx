4b60a00f3eb27be954e2c9270af59bca86b03d7f
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-10-13 15:47:08 +0200

    styling van updateForum en select velden op betreffende paginas aangepast

--- src/components/forumEdit/EditForum.jsx
+++ src/components/forumEdit/EditForum.jsx
@@ -1,6 +1,5 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import Button from "../button/Button.jsx";
 import styles from './ForumEdit.module.css';
 import ErrorMessage from "../errorMessage/ErrorMessage.jsx";
@@ -16,7 +15,6 @@ function EditForum({ forumId, forumData, onUpdate }) {
         }
     });
 
-    const navigate = useNavigate();
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [error, toggleError] = useState(false);
     const [topic, setTopic] = useState(forumData.topic);
@@ -38,9 +36,6 @@ function EditForum({ forumId, forumData, onUpdate }) {
             onUpdate(response.data);
             setIsSubmitted(true);
             console.log('isSubmitted set to true');
-            // setTimeout(() => {
-            //     navigate(`/forums/${forumId}`);
-            // }, 1000);
         } catch (e) {
             console.error(e);
             toggleError('Update niet gelukt, probeer het later opnieuw');
@@ -55,7 +50,7 @@ function EditForum({ forumId, forumData, onUpdate }) {
                 </div>
             ) : (
                 <div className={styles['edit-forum']}>
-                    <h3 className={styles['section-forum__comment-reactie']}>Update:</h3>
+                    <h3>Update:</h3>
                     <form onSubmit={handleSubmit(editForum)}>
                         <InputField
                             inputId="title-field"
@@ -66,7 +61,7 @@ function EditForum({ forumId, forumData, onUpdate }) {
                             errors={errors}
                         />
 
-                        <label htmlFor="text-field" className={styles['forum-form__textarea']}> Tekst:
+                        <label htmlFor="text-field"> Tekst:
                             <textarea
                                 name="text"
                                 id="text-field"
@@ -76,10 +71,10 @@ function EditForum({ forumId, forumData, onUpdate }) {
                             ></textarea>
                         </label>
 
-                        <label htmlFor="topic" className={styles['forum-form__topic']}> Selecteer het bijpassende
+                        <label htmlFor="topic"> Selecteer het bijpassende
                             onderwerp:
                             <select
-                                className={styles['forum-form__topic-select']}
+                                className={styles['topic-select']}
                                 id="topic"
                                 name="topic"
                                 value={topic}
